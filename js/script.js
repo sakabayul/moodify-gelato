91546bbf1b438d8a2ff46f3a690abe54b02caf8e
sakabayul <sakapkl015@gmail.com>
2023-06-03 20:01:33 +0700

    versi 1

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ const sr = ScrollReveal ({
     reset : true,
 })
 
-sr.reveal('.myimageabout',{ delay:300, origin:'left' })
-sr.reveal('.about-text-content',{ delay:300, origin:'right' })
-sr.reveal('.home-image',{ delay:300, origin:'top' })
-sr.reveal('.home-text-content',{ delay:300, origin:'top' })
-sr.reveal('.quality-content',{ delay:300, origin:'left' })
-sr.reveal('.qulity-image',{ delay:300, origin:'right' })
-sr.reveal('.gallery-image',{ delay:300, origin:'top' })
-sr.reveal('.inner-menu-food',{ delay:300, origin:'top' })
-sr.reveal('.menu-food-text',{ delay:300, origin:'bottom' })
-sr.reveal('.food-main-content',{ delay:300, origin:'bottom' })
-sr.reveal('.before',{ delay:300, origin:'bottom' })
+sr.reveal('.home-image',{ delay:400, origin:'top' })
+sr.reveal('.home-text-content',{ delay:1500, origin:'bottom' })
+sr.reveal('.about-text-content',{ delay:400, origin:'left' })
+sr.reveal('.quality-content',{ delay:400, origin:'bottom' })
+sr.reveal('.qulity-image',{ delay:1000, origin:'left' })
+sr.reveal('.gallery-image',{ delay:400, origin:'top' })
+sr.reveal('.food-main-content',{ delay:700, origin:'left' })
+sr.reveal('.inner-menu-food',{ delay:400, origin:'bottom' })
+sr.reveal('.menu-food-text',{ delay:700, origin:'top' })
+sr.reveal('.before',{ delay:400, origin:'top' })
+sr.reveal('.before-content',{ delay:1000, origin:'left' })
 
 
 
