6cf889edc68503c43de85455a6f1456b2fb790e7
sakabayul <sakapkl015@gmail.com>
2023-06-03 18:39:03 +0700

    first commit~

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 1500,
+    reset : true,
+})
+
+sr.reveal('.myimageabout',{ delay:300, origin:'left' })
+sr.reveal('.about-text-content',{ delay:300, origin:'right' })
+sr.reveal('.home-image',{ delay:300, origin:'top' })
+sr.reveal('.home-text-content',{ delay:300, origin:'top' })
+sr.reveal('.quality-content',{ delay:300, origin:'left' })
+sr.reveal('.qulity-image',{ delay:300, origin:'right' })
+sr.reveal('.gallery-image',{ delay:300, origin:'top' })
+sr.reveal('.inner-menu-food',{ delay:300, origin:'top' })
+sr.reveal('.menu-food-text',{ delay:300, origin:'bottom' })
+sr.reveal('.food-main-content',{ delay:300, origin:'bottom' })
+sr.reveal('.before',{ delay:300, origin:'bottom' })
+
+
+
+
+// let menubar = document.querySelector('#menubar');
+// let mynav = document.querySelector('.navbar');
+
+
+
+// menubar.onclick = () =>{
+//     menubar.classList.toggle('fa-times')
+//     mynav.classList.toggle('active')
+// }
+
+// document.querySelector('#loginbtn').onclick = () =>{
+//     document.querySelector('.login-form').classList.toggle('active');
+//   }
+//   document.querySelector('#closeloginform').onclick = () =>{
+//     document.querySelector('.login-form').classList.remove('active');
+//   }
